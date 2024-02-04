8585bb3b7ff5e8a899c4fdd6d434ae4968ab73f0
TinbiteIsayas <tinbiteisayas@TinbiteayassAir.home>
2024-02-04 14:39:27 +0100

    header and footer

---
+++ netflix-clone/src/pages/Home/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header/header'
+import Footer from '../../components/Footer/footer'
+
+const home = () => {
+  return (
+    <div>
+            <Header />
+            <Footer />
+    </div>
+  )
+}
+
+export default home
